5196f117e9557281209342fb318daed3264f1872
DanDaMan23 <danlawrcons.gmail.com>
2023-10-09 21:35:59 -0500

    reply endpoint created

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -69,3 +69,43 @@ export const newChat: RequestHandler = (req, res, next) => {
       next(err)
     })
 }
+
+export const reply: RequestHandler = (req, res, next) => {
+  const errors: Result<ValidationError> = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const error = new CustomError<ValidationError>(
+      422,
+      "Validation Failed",
+      errors.array()
+    )
+    throw error
+  }
+
+  type bodyType = {
+    message: string
+    chatId: string
+  }
+
+  const { message, chatId } = req.body as bodyType
+
+  Chat.findById(chatId)
+    .then((chat) => {
+      if (!chat) {
+        throw new CustomError(401, "No chat found with this Id")
+      }
+
+      const newMessage = new Message({
+        message,
+        creator: req.userId,
+        date: new Date()
+      })
+      return newMessage.save().then((result) => {
+        chat.messages.push(result._id)
+        return chat.save()
+      })
+    })
+    .then((result) => {
+      res.status(201).json({ message: "message sent", result })
+    })
+}
--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { check } from "express-validator"
-import { newChat } from "../controllers/chat"
+import { newChat, reply } from "../controllers/chat"
 import { isAuth } from "../middleware/is-auth"
 
 const router = Router()
@@ -15,7 +15,15 @@ router.post(
   newChat
 )
 
-router.put("/reply")
+router.put(
+  "/reply",
+  isAuth,
+  [
+    check("message", "Message must be 5 character long").isLength({ min: 5 }),
+    check("chatId", "Chat Id Required").notEmpty()
+  ],
+  reply
+)
 
 router.get("chat/:chatId")
 
